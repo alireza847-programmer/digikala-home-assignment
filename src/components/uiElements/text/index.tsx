0b604a3c4fca57ea47fe1ed1a4e9fb96e26fe776
alireza <alireza@alirezas-MacBook-Pro.local>
2024-02-08 13:17:34 +0330

    ADD DText component

---
+++ src/components/uiElements/text/index.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/native';
+import {TextProps} from 'types/components/uiElements/text';
+
+const VText = styled.Text<TextProps>(
+  ({
+    theme,
+    textAlign = 'center',
+    typography = 'medium15',
+    color = theme.colors.textColor.primary,
+    marginTopRatio = 0,
+    marginLeftRatio = 0,
+    fullWidth = false,
+    width,
+    minWidth,
+  }) => ({
+    color: color,
+    textAlign: textAlign,
+    marginTop: theme.verticalRem * marginTopRatio,
+    marginLeft: theme.verticalRem * marginLeftRatio,
+    textAlignVertical: 'center',
+    ...(fullWidth && {width: '100%'}),
+    ...theme.fonts[typography],
+    ...(width && {width}),
+    ...(minWidth && {minWidth}),
+  }),
+);
+
+export default VText;
